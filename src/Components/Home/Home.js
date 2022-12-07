d411718edf69107de00d8f6a1ef19bff93408ec4
SumiAkter2 <akteralisha36@gmail.com>
2022-12-07 10:51:56 +0600

    added navbar and toggle with dark mood

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="bg-primary h-full">
+      <h1>This is a Home</h1>
+    </div>
+  );
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#93c5fd",
+          secondary: "#374151",
+          accent: "#ffffff", // Navbar backGround Color
+          neutral: "#000000", // Navbar text
+          "base-100": "#ffffff",
+          info: "#1B1821",
+          success: "#000000", // backGround color
+          warning: "#1B1821", // button color
+          error: "#dc2626",
+        },
+      },
+      {
+        dark: {
+          primary: "#374151",
+          secondary: "#FDE68A",
+          accent: "#000000",
+          neutral: "#ffffff",
+          "base-100": "#1F2937",
+          info: "#fde047",
+          success: "#1BBB70",
+          warning: "#fcfafa",
+          error: "#be123c",
+        },
+      },
+    ],
   },
+
   plugins: [require("daisyui")],
 };
