9b521cc30a8ca48c42eea92a961a8ed01d35ae61
SumiAkter2 <akteralisha36@gmail.com>
2022-12-27 22:27:28 +0600

    added style in dropdown

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import { useAuthState, useSignOut } from "react-firebase-hooks/auth";
 import auth from "../../Firebase/firebase.init";
 import swal from "sweetalert";
 import "../Home/toggle.css";
-import "./Dashboard/Navbar.css";
+// import "./Dashboard/Navbar.css";
 const Navbar = ({ children, cart }) => {
   const [user] = useAuthState(auth);
   const [dark, setDark] = useState(false);
@@ -14,7 +14,28 @@ const Navbar = ({ children, cart }) => {
   cart.forEach((coin) => {
     quantity = quantity + coin.quantity;
   });
-
+  const navItems = [
+    <li>
+      <NavLink className="rounded-md h-12 my-auto" to="/">
+        Home
+      </NavLink>
+    </li>,
+    <li>
+      <NavLink className="rounded-md h-12 my-auto" to="/coin">
+        Coins
+      </NavLink>
+    </li>,
+    <li>
+      <NavLink className="rounded-md h-12 my-auto" to="/about">
+        About
+      </NavLink>
+    </li>,
+    <li>
+      <NavLink className="rounded-md h-12 my-auto" to="/contact">
+        Contact
+      </NavLink>
+    </li>,
+  ];
   return (
     <div data-theme={dark ? "dark" : "light"} className=" ">
       <div className="drawer drawer-end fixed top-0 ">
@@ -56,125 +77,117 @@ const Navbar = ({ children, cart }) => {
 
             <div className="flex-none hidden lg:block  ">
               <ul className="menu  menu-horizontal gap-x-2  ">
-                <li>
-                  <NavLink className="rounded-lg my-2" to="/">
-                    Home
-                  </NavLink>
-                </li>
-
-                <li>
-                  <NavLink className="rounded-lg  my-2  " to="/coin">
-                    Coins
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink className="rounded-lg  my-2  " to="/about">
-                    About
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink className="rounded-lg  my-2  " to="/contact">
-                    Contact
-                  </NavLink>
-                </li>
+                {navItems}
 
                 {user && (
                   <li>
-                    <NavLink className="rounded-lg  my-2  " to="/dashboard">
+                    <NavLink
+                      className="rounded-lg h-12 my-auto"
+                      to="/dashboard"
+                    >
                       Dashboard
                     </NavLink>
                   </li>
                 )}
-                {user ? (
+                {/* {user ? (
                   <>
                     <li>
-                      <button
-                        className=" rounded-lg  my-2  "
-                        onClick={async () => {
-                          const success = await signOut();
-                          if (success) {
-                            swal({
-                              icon: "success",
-                              text: "You are Sign Out Now",
-                            });
-                          }
-                        }}
-                      >
-                        Sign Out
-                      </button>
-                    </li>
-                    <li>
-                      {/* <NavLink>{user?.email}</NavLink>
-                      <NavLink>{user?.displayName}</NavLink> */}
+                      <NavLink>{user?.email}</NavLink>
+                      <NavLink>{user?.displayName}</NavLink>
                     </li>
                   </>
                 ) : (
                   <li>
-                    <NavLink className="rounded-lg  my-2  " to="/login">
+                    <NavLink className="rounded-lg   " to="/login">
                       Log In/Sign Up
                     </NavLink>
                   </li>
-                )}
+                )} */}
                 {/* =======================cart============= */}
                 <li>
-                  <NavLink className="rounded-lg  my-2 ">
+                  <NavLink className="rounded-lg h-12 my-auto">
                     <div className="dropdown dropdown-end ">
-                      <label
-                        tabIndex={0}
-                        className="  indicator cursor-pointer"
-                      >
-                        <HiOutlineShoppingCart size="25px" />
+                      <label tabIndex={0} className=" cursor-pointer indicator">
                         <span className="badge badge-sm indicator-item bg-primary text-gray-400">
                           {quantity}
                         </span>
+                        <HiOutlineShoppingCart
+                          size="25px"
+                          className=" hover:fill-gray-700 "
+                        />
                       </label>
                       <ul
                         tabIndex={0}
                         className="dropdown-content menu  shadow bg-base-100 rounded-box w-32 text-center"
                       >
-                        <li>
-                          <a>Item {quantity}</a>
-                        </li>
-                        <li className="text-center">
-                          <Link to="/carts">View Cart</Link>
-                        </li>
-                      </ul>
-                    </div>
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink className="rounded-lg  my-2 ">
-                    <div className="dropdown dropdown-end">
-                      <label
-                        tabIndex={0}
-                        className="avatar btn btn-ghost btn-circle "
-                      >
-                        <div className="w-10 rounded-full   cursor-pointer">
-                          <img
-                            className=""
-                            src="https://placeimg.com/80/80/people"
-                            alt="profile-img"
-                          />
-                          <span className="badge badge-xs bg-green-600 indicator-item"></span>
+                        <div className="mx-auto">
+                          <li>
+                            <p>Item {quantity}</p>
+                          </li>
+                          <li>
+                            <Link to="/carts">View Cart</Link>
+                          </li>
                         </div>
-                      </label>
-                      <ul
-                        tabIndex={0}
-                        className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-48"
-                      >
-                        <li>
-                          <a className="justify-between">Profile</a>
-                        </li>
-                        <li>
-                          <a>Settings</a>
-                        </li>
-                        <li>
-                          <a>Logout</a>
-                        </li>
                       </ul>
                     </div>
                   </NavLink>
                 </li>
+                {user ? (
+                  <>
+                    <li>
+                      <NavLink className="rounded-lg h-12 my-auto">
+                        <div className="dropdown dropdown-end">
+                          <label
+                            tabIndex={0}
+                            className="avatar cursor-pointer btn btn-ghost"
+                          >
+                            <div className="w-10 rounded-full   cursor-pointer">
+                              <img
+                                className=""
+                                src="https://i.ibb.co/5sWZQdg/default-images.jpg"
+                                alt="profile-img"
+                              />
+                              <span className="badge badge-xs bg-green-600 indicator-item"></span>
+                            </div>
+                          </label>
+                          <ul
+                            tabIndex={0}
+                            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-48 mx-auto"
+                          >
+                            <div className="mx-auto">
+                              <li>
+                                <a className="text-center">Profile</a>
+                              </li>
+                              <li>
+                                <a>Settings</a>
+                              </li>
+                              <li>
+                                <button
+                                  className=" rounded-lg  my-2  "
+                                  onClick={async () => {
+                                    const success = await signOut();
+                                    if (success) {
+                                      swal({
+                                        icon: "success",
+                                        text: "You are Sign Out Now",
+                                      });
+                                    }
+                                  }}
+                                >
+                                  Sign Out
+                                </button>
+                              </li>
+                            </div>
+                          </ul>
+                        </div>
+                      </NavLink>
+                    </li>
+                  </>
+                ) : (
+                  <li>
+                    <NavLink to="/login"> Log In</NavLink>
+                  </li>
+                )}
               </ul>
             </div>
           </div>
