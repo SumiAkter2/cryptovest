47aa069b363852a273ef583b8af35a5db01531ac
SumiAkter2 <akteralisha36@gmail.com>
2022-12-29 14:28:13 +0600

    added

--- src/Components/Subscribe/Subscribe.js
+++ src/Components/Subscribe/Subscribe.js
@@ -3,7 +3,7 @@ import React from "react";
 const Subscribe = () => {
   return (
     <div className="bg-primary lg:py-12 lg:m-12 m-6">
-      <div className=" lg:flex justify-evenly items-center my-24 bg-accent p-12 m-12 rounded-md ">
+      <div className=" lg:flex justify-evenly items-center my-24 bg-accent p-12 lg:m-12 rounded-md ">
         <div>
           <h1 className="text-3xl text-center text-primary">
             SIGN UP TO NEWSLETTER 
